707e3be8c41a3facc576cf07958d7e60a98da90f
CroutonUndercover <vpmitsev@gmail.com>
2019-11-12 21:19:23 +0200

    build

---
+++ build/precache-manifest.3dcaccbbec8b5f78ba106ca421bc4be2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b946d8fc54fe83b5a36bc421b06c35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a31b7a6a05c0781a73ce",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "83eb30fbb258ebb02ed5",
+    "url": "/static/css/main.7d3ef981.chunk.css"
+  },
+  {
+    "revision": "a31b7a6a05c0781a73ce",
+    "url": "/static/js/2.beaf7e86.chunk.js"
+  },
+  {
+    "revision": "83eb30fbb258ebb02ed5",
+    "url": "/static/js/main.68d72f4a.chunk.js"
+  },
+  {
+    "revision": "4782b60207cf30a2fdd5",
+    "url": "/static/js/runtime-main.b37a0f03.js"
+  },
+  {
+    "revision": "0596c0f7f37b8ad4a9c14facc8f130d9",
+    "url": "/static/media/braveheart_dafont.0596c0f7.woff"
+  }
+]);
